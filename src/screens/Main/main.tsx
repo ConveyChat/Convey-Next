58b3cc68b8d5ee773f3cfb72f1451275edf7712d
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-05-18 16:35:54 -0400

    Added layout components + grid CSS

--- src/screens/Main/main.tsx
+++ src/screens/Main/main.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
-import './main.css'
+import Sidebar from '../../components/Sidebar/sidebar'
+import MsgList from '../../components/MsgList/msglist'
+import MsgViewer from '../../components/MsgViewer/msgviewer'
+
+import Styles from './main.module.css'
 
 export default function Main() {
     return (
-        <div>
-            <h1>asdfasdfasdf</h1>
+        <div className={Styles.app}>
+            <div className={Styles.app__sidebar}>
+                <Sidebar />
+            </div>
+            <div className={Styles.app__msglist}>
+                <MsgList />
+            </div>
+            <div className={Styles.app__msgviewer}>
+                <MsgViewer />
+            </div>
         </div>
     )
 }
