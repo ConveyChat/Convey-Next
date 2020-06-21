d4b84828326248d9df22bff46185d736e5a462dc
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-06-21 14:27:24 -0400

    updated batch file to delete problematic node ts definitions on windows

--- fixNodeTypes.bat
+++ fixNodeTypes.bat
@@ -1,1 +1,4 @@
 @RD /S /Q "%~dp0\node_modules\@types\bn.js\node_modules"
+@RD /S /Q "%~dp0\node_modules\@types\body-parser\node_modules"
+@RD /S /Q "%~dp0\node_modules\@types\connect\node_modules"
+@RD /S /Q "%~dp0\node_modules\@types\express-serve-static-core\node_modules"
