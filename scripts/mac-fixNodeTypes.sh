a5ba2522638f2c6dc599fb5976600f89653825bb
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-06-28 16:29:17 -0400

    Added shell script to fix node types

---
+++ scripts/mac-fixNodeTypes.sh
@@ -0,0 +1,4 @@
+rm -r ../node_modules/@types/bn.js/node_modules
+rm -r ../node_modules/@types/body-parser/node_modules
+rm -r ../node_modules/@types/connect/node_modules
+rm -r ../node_modules/@types/express-serve-static-core/node_modules
